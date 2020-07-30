4308d2142f87584b16d0b54bb439aa48a9bb0b2d
KP Parrish <57117452+KParrish193@users.noreply.github.com>
2020-07-30 15:44:26 -0600

    backend fleshed out with artists and prints from google form

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '5d4b6fbff98da50503a17c6e53a79981'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'a9229d46-e089-4a8d-916b-a65525a1a0bb'
+};
