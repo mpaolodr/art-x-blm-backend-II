4cabaeeccbcc111aa84e003edd040e4e1d08c012
KP Parrish <57117452+KParrish193@users.noreply.github.com>
2020-07-31 12:45:34 -0600

    update database config

--- backend/config/database.js
+++ backend/config/database.js
@@ -5,11 +5,11 @@ module.exports = ({ env }) => ({
       connector: 'bookshelf',
       settings: {
         client: 'postgres',
-        host: env('DATABASE_HOST', '127.0.0.1'),
+        host: env('DATABASE_HOST', 'ec2-52-22-216-69.compute-1.amazonaws.com'),
         port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'artxblm-backend'),
-        username: env('DATABASE_USERNAME', 'postgres'),
-        password: env('DATABASE_PASSWORD', 'Riotatchya_119'),
+        database: env('DATABASE_NAME', 'd8alf75tndhic8'),
+        username: env('DATABASE_USERNAME', 'lbmhutmnkbhxhk'),
+        password: env('DATABASE_PASSWORD', '7def99131b6e8850eb153e9746c4edcb20f308a5f8e8ef6597894375756b0628'),
         ssl: env.bool('DATABASE_SSL', false),
       },
       options: {}
