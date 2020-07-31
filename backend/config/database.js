3d79e15d86f4bba1a9493f107fa491a7a242ea63
KP Parrish <57117452+KParrish193@users.noreply.github.com>
2020-07-31 14:02:03 -0600

    added env file

--- backend/config/database.js
+++ backend/config/database.js
@@ -5,11 +5,11 @@ module.exports = ({ env }) => ({
       connector: 'bookshelf',
       settings: {
         client: 'postgres',
-        host: env('DATABASE_HOST', 'ec2-52-22-216-69.compute-1.amazonaws.com'),
-        port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'd8alf75tndhic8'),
-        username: env('DATABASE_USERNAME', 'lbmhutmnkbhxhk'),
-        password: env('DATABASE_PASSWORD', '7def99131b6e8850eb153e9746c4edcb20f308a5f8e8ef6597894375756b0628'),
+        host: env('DATABASE_HOST', process.env.DATABASE_HOST),
+        port: env.int('DATABASE_PORT', process.env.DATABASE_PORT),
+        database: env('DATABASE_NAME', process.env.DATABASE_NAME),
+        username: env('DATABASE_USERNAME', process.env.DATABASE_USERNAME),
+        password: env('DATABASE_PASSWORD', process.env.DATABASE_PASSWORD),
         ssl: env.bool('DATABASE_SSL', false),
       },
       options: {}
