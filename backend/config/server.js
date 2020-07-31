85066fa711723e685091cda0085ba9b7aef0f4ff
KP Parrish <57117452+KParrish193@users.noreply.github.com>
2020-07-31 13:07:02 -0600

    deploy

--- backend/config/server.js
+++ backend/config/server.js
@@ -2,6 +2,7 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   admin: {
+    url: '/dashboard',
     auth: {
       secret: env('ADMIN_JWT_SECRET', '5d4b6fbff98da50503a17c6e53a79981'),
     },
