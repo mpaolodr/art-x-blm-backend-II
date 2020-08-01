30039fc4643c110d65e9a3de35b789b125a0bb5a
mpaolodr <mpaolodr_1504@yahoo.com>
2020-08-01 14:03:55 -0400

    env vars

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,18 +1,18 @@
 module.exports = ({ env }) => ({
-  defaultConnection: 'default',
+  defaultConnection: "default",
   connections: {
     default: {
-      connector: 'bookshelf',
+      connector: "bookshelf",
       settings: {
-        client: 'postgres',
-        host: env('DATABASE_HOST', process.env.DATABASE_HOST),
-        port: env.int('DATABASE_PORT', process.env.DATABASE_PORT),
-        database: env('DATABASE_NAME', process.env.DATABASE_NAME),
-        username: env('DATABASE_USERNAME', process.env.DATABASE_USERNAME),
-        password: env('DATABASE_PASSWORD', process.env.DATABASE_PASSWORD),
-        ssl: env.bool('DATABASE_SSL', false),
+        client: "postgres",
+        host: env("DATABASE_HOST", process.env.DATABASE_HOST),
+        port: env.int("DATABASE_PORT", process.env.DATABASE_PORT),
+        database: env("DATABASE_NAME", process.env.DATABASE_NAME),
+        username: env("DATABASE_USERNAME", process.env.DATABASE_USERNAME),
+        password: env("DATABASE_PASSWORD", process.env.DATABASE_PASSWORD),
+        ssl: env.bool("DATABASE_SSL", process.env.DATABASE_SSL),
       },
-      options: {}
+      options: {},
     },
   },
 });
