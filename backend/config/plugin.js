e56ab425498f273141f448166f19963e02d19b7c
mpaolodr <mpaolodr_1504@yahoo.com>
2020-08-04 23:31:38 -0400

    test

--- backend/config/plugin.js
+++ backend/config/plugin.js
@@ -2,9 +2,9 @@ module.exports = ({ env }) => ({
   upload: {
     provider: "cloudinary",
     providerOptions: {
-      cloud_name: env("CLOUDINARY_NAME"),
-      api_key: env("CLOUDINARY_KEY"),
-      api_secret: env("CLOUDINARY_SECRET"),
+      cloud_name: env("CLOUDINARY_NAME", process.env.CLOUDINARY_NAME),
+      api_key: env("CLOUDINARY_KEY", process.env.CLOUDINARY_KEY),
+      api_secret: env("CLOUDINARY_SECRET", process.env.CLOUDINARY_SECRET),
     },
   },
 });
